4a41da2b5bdb9eaede2d25b7ff829df41789878f
Rickard Holmberg <rickard@holmberg.info>
2012-09-29 10:11:10 +0200

    Fix breakage from last commit, embrynonic RT Dose support

--- create_ct.py
+++ create_ct.py
@@ -18,9 +18,9 @@ def generate_uid(_uuid = None):
 
 ImplementationClassUID = '2.25.229451600072090404564544894284998027172'
 
-def get_empty_dataset(filename):
+def get_empty_dataset(filename, storagesopclass):
     file_meta = dicom.dataset.Dataset()
-    file_meta.MediaStorageSOPClassUID = get_uid("CT Image Storage")
+    file_meta.MediaStorageSOPClassUID = get_uid(storagesopclass)
     file_meta.MediaStorageSOPInstanceUID = generate_uid()
     file_meta.ImplementationClassUID = ImplementationClassUID
     ds = dicom.dataset.FileDataset(filename, {}, file_meta=file_meta, preamble="\0"*128)
@@ -29,20 +29,37 @@ def get_empty_dataset(filename):
 def get_default_ct_dataset(filename):
     DT = "%04i%02i%02i" % datetime.datetime.now().timetuple()[:3]
     TM = "%02i%02i%02i" % datetime.datetime.now().timetuple()[3:6]
-    ds = get_empty_dataset(filename)
-    get_sop_commom_module(ds)
+    ds = get_empty_dataset(filename, "CT Image Storage")
+    get_sop_common_module(ds, DT, TM)
     get_ct_image_module(ds)
     get_image_pixel_macro(ds)
     get_patient_module(ds)
-    get_general_study_module(ds)
-    get_general_series_module(ds)
+    get_general_study_module(ds, DT, TM)
+    get_general_series_module(ds, DT, TM)
     get_frame_of_reference_module(ds)
     get_general_equipment_module(ds)
-    get_general_image_module(ds)
+    get_general_image_module(ds, DT, TM)
     get_image_plane_module(ds)
     return ds
 
-def get_sop_commom_module(ds):
+def get_default_rt_dose_dataset(filename):
+    DT = "%04i%02i%02i" % datetime.datetime.now().timetuple()[:3]
+    TM = "%02i%02i%02i" % datetime.datetime.now().timetuple()[3:6]
+    ds = get_empty_dataset(filename, "RT Plan Storage")
+    get_sop_common_module(ds, DT, TM)
+    get_patient_module(ds)
+    get_image_pixel_macro(ds)
+    get_general_study_module(ds, DT, TM)
+    get_general_series_module(ds, DT, TM)
+    get_frame_of_reference_module(ds)
+    get_general_equipment_module(ds)
+    get_general_image_module(ds, DT, TM)
+    get_image_plane_module(ds)
+    get_multi_frame_module(ds)
+    get_rt_dose_module(ds)
+    return ds
+    
+def get_sop_common_module(ds, DT, TM):
     # Type 1
     ds.SOPClassUID = get_uid("CT Image Storage")
     ds.SOPInstanceUID = ""
@@ -77,7 +94,7 @@ def get_patient_module(ds):
     ds.PatientsBirthDate = ""
     ds.PatientsSex = "O"
 
-def get_general_study_module(ds):
+def get_general_study_module(ds, DT, TM):
     # Type 1
     ds.StudyInstanceUID = ""
     # Type 2
@@ -89,7 +106,7 @@ def get_general_study_module(ds):
     # Type 3
     #ds.StudyDescription = ""
 
-def get_general_series_module(ds):
+def get_general_series_module(ds, DT, TM):
     # Type 1
     ds.Modality = "CT"
     ds.SeriesInstanceUID = ""
@@ -114,7 +131,7 @@ def get_general_equipment_module(ds):
     ds.ManufacturersModelName = "https://github.com/raysearchlabs/dicomutils"
     ds.SoftwareVersions = "PyDICOM %s" % (dicom.__version__,)
 
-def get_general_image_module(ds):
+def get_general_image_module(ds, DT, TM):
     # Type 2
     ds.InstanceNumber = ""
     # Type 3
@@ -133,17 +150,81 @@ def get_image_plane_module(ds):
     ds.SliceThickness = 1.0
     # Type 3
     # ds.SliceLocation = 0
+
+def get_multi_frame_module(ds):
+    # Type 1
+    ds.NumberofFrames = 1
+    ds.FrameIncrementPointer = dicom.datadict.Tag(dicom.datadict.tag_for_name("GridFrameOffsetVector"))
+
+def get_rt_dose_module(ds):
+    # Type 1C on PixelData
+    ds.SamplesperPixel = 1
+    ds.ImageType = "ORIGINAL\SECONDARY\AXIAL"
+    ds.SamplesperPixel = 1
+    ds.PhotometricInterpretation = "MONOCHROME2"
+    ds.BitsAllocated = 16
+    ds.BitsStored = 16
+    ds.HighBit = 15
+    ds.PixelRepresentation = 0
+
+    # Type 1
+    ds.DoseUnits = "GY"
+    ds.DoseType = "PHYSICAL"
+    ds.DoseSummationType = "PLAN"
+
+    # Type 1C if Dose Summation Type is any of the enumerated values. 
+    ds.ReferencedRTPlanSequence = [dicom.dataset.Dataset()]
+    ds.ReferencedRTPlanSequence[0].ReferencedSOPClassUID = get_uid("RT Plan Storage")
+    ds.ReferencedRTPlanSequence[0].ReferencedSOPInstanceUID = generate_uid()
+
+    # Type 1C on multi-frame
+    ds.GridFrameOffsetVector = [0,1,2,3,4]
+
+    # Type 1C
+    if (ds.DoseSummationType == "FRACTION" or
+        ds.DoseSummationType == "BEAM" or
+        ds.DoseSummationType == "BRACHY" or
+        ds.DoseSummationType == "CONTROL_POINT"):
+        ds.ReferencedRTPlanSequence[0].ReferencedFractionGroupSequence = [dicom.dataset.Dataset()]
+        # Type 1
+        ds.ReferencedRTPlanSequence[0].ReferencedFractionGroupSequence[0].ReferencedFractionGroupNumber = 0
+        # Type 1C
+        if (ds.DoseSummationType == "BEAM" or
+            ds.DoseSummationType == "CONTROL_POINT"):
+            ds.ReferencedRTPlanSequence[0].ReferencedFractionGroupSequence[0].ReferencedBeamSequence = [dicom.dataset.Dataset()]
+            # ... and on it goes...
+            raise NotImplementedError
+        elif ds.DoseSummationType == "BRACHY":
+            raise NotImplementedError
     
-def write_ct(filenamebase, ctData, voxelGrid, **kwargs):
+    # Type 3
+    # ds.InstanceNumber = 0
+    # ds.DoseComment = "blabla"
+    # ds.NormalizationPoint = [0,0,0]
+    # ds.TissueHeterogeneityCorrection = "IMAGE" # or "ROI_OVERRIDE" or "WATER"
+
+def write_rt_dose(doseData, voxelGrid, **kwargs):
+    nVoxels = ctData.shape
+    rtdoseuid = generate_uid()
+    FoRuid = generate_uid()
+    studyuid = generate_uid()
+    seriesuid = generate_uid()
+    filename = "RD_%s.dcm" % (rtdoseuid,)
+    rd = get_default_rt_dose_dataset(filename)
+    
+    
+    
+def write_ct(ctData, voxelGrid, **kwargs):
     nVoxels = ctData.shape
     ctbaseuid = generate_uid()
     FoRuid = generate_uid()
     studyuid = generate_uid()
     seriesuid = generate_uid()
     for z in range(nVoxels[2]):
-        filename = "%s-%i.dcm" % (filenamebase, z)
+        sopinstanceuid = "%s.%i" % (ctbaseuid, z)
+        filename = "CT_%s.dcm" % (sopinstanceuid,)
         ct = get_default_ct_dataset(filename)
-        ct.SOPInstanceUID = "%s.%i" % (ctbaseuid, z)
+        ct.SOPInstanceUID = sopinstanceuid
         ct.SeriesInstanceUID = seriesuid
         ct.StudyInstanceUID = studyuid
         ct.FrameofReferenceUID = FoRuid
@@ -170,8 +251,6 @@ def get_centered_coordinates(voxelGrid, nVoxels):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Create DICOM CT data.')
-    parser.add_argument('filenameBase', metavar='filename-base', 
-                        help='The base of the generated filenames. For example, specifying "foo" gives files called "foo-0.dcm", "foo-1.dcm" etc.') 
     parser.add_argument('--patient-position', dest='PatientPosition', choices = ['HFS', 'HFP', 'FFS', 'FFP', 'HFDR', 'HFDL', 'FFDR', 'FFDP'],
                         help='The patient position written in the images (default: not specified)')
     parser.add_argument('--voxelsize', dest='VoxelSize', default="1,2,4",
@@ -193,4 +272,4 @@ if __name__ == '__main__':
     ctData -= 1000*(numpy.sqrt(x**2+y**2+z**2) < 30)
 
 
-    write_ct(args.filenameBase, ctData, voxelGrid, PatientPosition = args.PatientPosition)
+    write_ct(ctData, voxelGrid, PatientPosition = args.PatientPosition)
